3642ebc86cae653d61b5f31ddbb3b7814a197ecd
Róbert Selvek <robert.selvek@gmail.com>
2013-03-16 16:39:13 +0100

    Added documentation to info.go

--- updater/info.go
+++ updater/info.go
@@ -7,12 +7,23 @@ import (
 	"net/http"
 )
 
+//Info is a struct used to represent information about
+//updated broadcast and config on the server.
 type Info struct {
+	//Broadcast informs whether there is
+	//a newer broadcast than the one client has.
 	Broadcast bool
-	FileType  string
-	Config    bool
+
+	//FileType gives the type of the broadcast file.
+	FileType string
+
+	//Config informs whether there is a newer version
+	//of centralConfig available.
+	Config bool
 }
 
+//GetInfo returns an Info struct with current
+//information from server.
 func GetInfo(c *config.Config) (i Info, err error) {
 	r, err := downloadInfo(c.UpdateURL + "/update?client=" + c.Name)
 	if err != nil {
@@ -23,6 +34,7 @@ func GetInfo(c *config.Config) (i Info, err error) {
 	return parseInfo(r)
 }
 
+//downloadInfo downloads the update JSON from the server. 
 func downloadInfo(url string) (io.ReadCloser, error) {
 	resp, err := http.Get(url)
 	if err != nil {
@@ -31,18 +43,23 @@ func downloadInfo(url string) (io.ReadCloser, error) {
 	return resp.Body, nil
 }
 
+//parseInfo parses the JSON info from the server.
 func parseInfo(r io.Reader) (i Info, err error) {
 	d := json.NewDecoder(r)
 	err = d.Decode(&i)
 	return
 }
 
+//AnnounceBroadcast announces the completition of download 
+//of the broadcast to the server.
 func AnnounceBroadcast(c *config.Config) error {
 	url := c.UpdateURL + "/presentation/active/downloadComplete?client=" + c.Name
 	_, err := http.Get(url)
 	return err
 }
 
+//AnnounceConfig announces the completition of download 
+//of the centralConfig to the server.
 func AnnounceConfig(c *config.Config) error {
 	url := c.UpdateURL + "/config/downloadComplete?client=" + c.Name
 	_, err := http.Get(url)
