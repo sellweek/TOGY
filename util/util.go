d4023a1cba0cd1e3abd57129d2155f99f17b69c9
Róbert Selvek <robert.selvek@gmail.com>
2013-06-04 17:38:34 +0200

    Added forceTZ to Normalizing functions

--- util/util.go
+++ util/util.go
@@ -6,6 +6,8 @@ import (
 	"time"
 )
 
+var Tz, _ = time.LoadLocation("Europe/Bratislava")
+
 //Returns true if the file on path a was modified later than the file on path b.
 //If an error is encountered, returns false and the error.
 func IsNewer(a, b string) (bool, error) {
@@ -22,12 +24,32 @@ func IsNewer(a, b string) (bool, error) {
 	return fia.ModTime().After(fib.ModTime()), nil
 }
 
-func NormalizeDate(t time.Time) time.Time {
-	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, time.UTC)
+//NormalizeDate strips the time part from time.Date leaving only
+//year, month and day.
+//If forceTZ is true, its location will be set to util.Tz,
+//if false, it will be left as is.
+func NormalizeDate(t time.Time, forceTZ bool) time.Time {
+	var tz *time.Location
+	if forceTZ {
+		tz = Tz
+	} else {
+		tz = t.Location()
+	}
+	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, tz)
 }
 
-func NormalizeTime(t time.Time) time.Time {
-	return time.Date(0, 1, 1, t.Hour(), t.Minute(), t.Second(), t.Nanosecond(), time.UTC)
+//NormalizeTime strips the date part from time.Date leaving only
+//hours, minutes, seconds and nanoseconds.
+//If forceTZ is true, its location will be set to util.Tz,
+//if false, it will be left as is.
+func NormalizeTime(t time.Time, forceTZ bool) time.Time {
+	var tz *time.Location
+	if forceTZ {
+		tz = Tz
+	} else {
+		tz = t.Location()
+	}
+	return time.Date(0, 1, 1, t.Hour(), t.Minute(), t.Second(), t.Nanosecond(), tz)
 }
 
 func GetFileType(filename string) string {
